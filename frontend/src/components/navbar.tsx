d63334e7c88d8ff2c85d141c644be68a77ae9a7e
semir <semir.salkic1@gmail.com>
2023-01-04 15:11:17 +0100

    fixed:navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
 import esproLogo from '../assets/espro_logo.svg';
 import './navbar.scss';
 
@@ -9,29 +9,37 @@ export default class NavbarComponent extends React.Component {
       <div className="px-0 py-0 fixed-top">
         <div id="offerBanner">
           <Navbar>
-            <Row className="container-fluid mx-0 px-0 py-0 my-0 text-center offerBanner">
-              <div className="py-3">FREE SHIPPING ON ALL U.S. ORDERS $49+</div>
+            <Row className="container-fluid py-2 text-center offerBanner">
+              <div className="py-">FREE SHIPPING ON ALL U.S. ORDERS $49+</div>
             </Row>
           </Navbar>
         </div>
-        <Navbar className="navbarMain bg-white" collapseOnSelect expand="sm">
-          <div className=" container-fluid navbarMain mx-5 px-5  py-2">
-            <Nav>
-              <NavDropdown className="mx-5" title="shop" id="shopDropdown">
-                <NavDropdown.Item>Action1</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link>OUR STORY</Nav.Link>
-            </Nav>
-            <Nav>
-              <Navbar.Brand href="/" className="me-0">
+
+        <Navbar bg="light" expand="lg" className=" px-5 py-3">
+          <Container fluid>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse>
+              <Nav>
+                <NavDropdown title="shop" id="shopDropdown">
+                  <NavDropdown.Item>Action1</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link>our story</Nav.Link>
+              </Nav>
+              <Navbar.Brand href="/" className="mx-auto">
                 <img src={esproLogo} alt="" />
               </Navbar.Brand>
-            </Nav>
-            <Nav>
-              <Nav.Link className="mx-5">BREW GUIDES</Nav.Link>
-              <Nav.Link>BLOG</Nav.Link>
-            </Nav>
-          </div>
+              <Nav>
+                <Nav>
+                  <Nav.Link>brew guides</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link>blog</Nav.Link>
+                </Nav>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
         </Navbar>
       </div>
     );
